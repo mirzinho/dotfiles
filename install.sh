560be28f1497986ece5f2b23e5c9b9caa8de524d
Ivan Brčić <ivan.brcic@hibob.io>
2024-09-18 09:15:44 +0000

    WIP

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Install latest static tmux
+sudo curl -L https://github.com/axonasif/build-static-tmux/releases/latest/download/tmux.linux-amd64.stripped -o /usr/bin/tmux
+sudo chmod +x /usr/bin/tmux
+
+# Auto start tmux session
+current_dir="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
+dotfiles_source="${current_dir}/home_files"
+
+# Symlink dotfiles
+while read -r file; do
+
+    relative_file_path="${file#"${dotfiles_source}"/}"
+    target_file="${HOME}/${relative_file_path}"
+    target_dir="${target_file%/*}"
+
+    if test ! -d "${target_dir}"; then
+        mkdir -p "${target_dir}"
+    fi
+
+    printf 'Installing dotfiles symlink %s\n' "${target_file}"
+    ln -sf "${file}" "${target_file}"
+
+done < <(find "${dotfiles_source}" -type f)
+
+
+git config --global push.default current
+
