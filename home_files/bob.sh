3900d8b6a1dbeecd97bf8471c2f3a72975c43c02
mirzinho <mirzinho@gmail.com>
2025-03-17 16:20:34 +0100

    Update bob.sh

--- home_files/bob.sh
+++ home_files/bob.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 # Check if the 'hiring' session exists
-if ! tmux has-session -t hiring 2>/dev/null; then
+if ! tmux has-session -t bob 2>/dev/null; then
   # Create the session and split the window horizontally
-  tmux new-session -d -s hiring \; \
+  tmux new-session -d -s bob \; \
     split-window -h \; \
-    send-keys -t hiring:0.0 'npm run bob' C-m
+    send-keys -t bob:0.0 'npm run bob' C-m
 
   # Attach to the new session
-  tmux attach -t hiring
+  tmux attach -t bob
 else
   # If the session exists, attach to it
-  tmux attach -t hiring
+  tmux attach -t bob
 fi
